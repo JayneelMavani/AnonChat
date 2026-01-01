4986a2342a062ad4ff304096b34a298411bfce7b
Hetul Mistry <85955968+HetulMistry@users.noreply.github.com>
2026-01-01 08:48:14 +0000

    feat: Username generator update and username save to localstorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
 
 const STORAGE_KEY = "chat_username";
@@ -12,9 +13,7 @@ const generateUsername = async () => {
     const nouns = await fetch("https://api.datamuse.com/words?rel_trg=animal").then(res => res.json());
     const noun = nouns[Math.floor(Math.random() * nouns.length)];
 
-    const number = Math.floor(Math.random() * 1000);
-
-    return `anonymous-${adjective.word}-${noun.word}-${number}`;
+    return `anonymous-${adjective.word}-${noun.word}-${nanoid(5)}`;
   } catch (error) {
     console.error(`Error generating username: ${error}`);
   }
@@ -23,18 +22,37 @@ const generateUsername = async () => {
 export default function Home() {
   const [username, setUsername] = useState<string>("");
 
-  const updateUsername = async () => {
+  // const updateUsername = async () => {
+  //   const newUsername = await generateUsername();
+  //   setUsername(newUsername || '');
+  // };
+
+  const genAndStoreUsername = async () => {
+    const stored = localStorage.getItem(STORAGE_KEY);
+
+    if (stored) {
+      setUsername(stored);
+      return;
+    }
+
     const newUsername = await generateUsername();
-    setUsername(newUsername || '');
+    if (newUsername) {
+      localStorage.setItem(STORAGE_KEY, newUsername);
+      setUsername(newUsername);
+    }
   };
 
   useEffect(() => {
-    updateUsername();
+    genAndStoreUsername();
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="w-full max-w-md space-y-8">
+
+        <div className="text-center space-y-2">
+          <h1 className="text-2xl font-bold tracking-tight text-green-500">private_chat</h1>
+        </div>
         <div className="border border-zinc-800 bg-zinc-900/50 p-6 backdrop-blur-md">
           <div className="space-y-5">
             <div className="space-y-2">
